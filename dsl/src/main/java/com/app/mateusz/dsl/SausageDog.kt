59e958ec22683053d0183f9b3776dbd0fd2b31c5
trOnk12 <m.pachulski94@gmail.com>
2020-08-29 22:15:49 +0200

    make SausageDog immutable and instead use builder that hides the immutability

--- dsl/src/main/java/com/app/mateusz/dsl/SausageDog.kt
+++ dsl/src/main/java/com/app/mateusz/dsl/SausageDog.kt
@@ -1,26 +1,60 @@
 package com.app.mateusz.dsl
 
 data class SausageDog(
-    var dogAppearence: DogAppearence? = null,
-    var owner: Owner? = null
+    val dogAppearance: DogAppearance? = null,
+    val owner: Owner? = null
 )
 
-data class DogAppearence(
-    var name: String? = "",
-    var age: Int? = 0,
-    var color: String? = "",
-    var length: Double? = 0.00,
-    var owner: Owner? = null
+data class DogAppearance(
+    val name: String? = "",
+    val age: Int? = 0,
+    val color: String? = "",
+    val length: Double? = 0.00
 )
 
 data class Owner(var name: String? = "", var country: String? = "")
 
-fun sausagesDog(block: SausageDog.() -> Unit) = SausageDog().apply(block)
+fun sausagesDog(block: SausageDogBuilder.() -> Unit) = SausageDogBuilder().apply(block).build()
 
-fun SausageDog.owner(block: Owner.() -> Unit) {
-    owner = Owner().apply(block)
+class SausageDogBuilder {
+
+    private var dogAppearance: DogAppearance? = null
+    private var owner: Owner? = null
+
+    fun owner(block: OwnerBuilder.() -> Unit) {
+        owner = OwnerBuilder().apply(block).build()
+    }
+
+    fun dogAppearance(block: DogAppearanceBuilder.() -> Unit) {
+        dogAppearance = DogAppearanceBuilder().apply(block).build()
+    }
+
+    fun build(): SausageDog {
+        return SausageDog(dogAppearance, owner)
+    }
+
 }
 
-fun SausageDog.dogAppearance(block: DogAppearence.() -> Unit) {
-    dogAppearence = DogAppearence().apply(block)
+class DogAppearanceBuilder {
+
+    var name: String? = ""
+    var age: Int? = 0
+    var color: String? = ""
+    var length: Double? = 00.00
+
+    fun build(): DogAppearance {
+        return DogAppearance(name, age, color, length)
+    }
+
+}
+
+class OwnerBuilder {
+
+    var name: String? = ""
+    var country: String? = ""
+
+    fun build(): Owner {
+        return Owner(name, country)
+    }
+
 }
