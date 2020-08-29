d1e55cb584e7ee447950be15aaf13bc5549624b5
trOnk12 <m.pachulski94@gmail.com>
2020-08-29 15:29:24 +0200

    init

---
+++ coroutine/src/main/java/com/app/mateusz/coroutine/CoroutinePlayground.kt
@@ -0,0 +1,13 @@
+package com.app.mateusz.coroutine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CoroutinePlayground : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coroutine_playground)
+
+    }
+}
