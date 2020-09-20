dec6c6c8ccc4995922934f6d10f834af893a90d4
trOnk12 <m.pachulski94@gmail.com>
2020-09-20 14:50:07 +0200

    ensureActive() is needed when cancelling the job to stop the work executed in the coroutine

--- coroutine/src/main/java/com/app/mateusz/coroutine/CoroutinePlaygroundActivity.kt
+++ coroutine/src/main/java/com/app/mateusz/coroutine/CoroutinePlaygroundActivity.kt
@@ -4,24 +4,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_coroutine_playground.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
 
 class CoroutinePlaygroundActivity : AppCompatActivity(), CoroutineScope {
 
     override val coroutineContext: CoroutineContext
-        get() = Dispatchers.Default
+        get() = Dispatchers.IO
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coroutine_playground)
 
        val job = launch {
-            for(i in 1..1000000)
-            Log.d("TEST","value: $i on thread ${Thread.currentThread()}")
+           withContext(Dispatchers.Default) {
+
+               for (i in 1..1000000) {
+                   ensureActive()
+                       Log.d("TEST", "value: $i on thread ${Thread.currentThread()}")
+                   }
+
+           }
         }
+
+        job.invokeOnCompletion { Log.d("TEST","job completed with ${it?.localizedMessage}") }
 
         button.setOnClickListener {
             job.cancel()
